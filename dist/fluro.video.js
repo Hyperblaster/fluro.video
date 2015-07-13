d287d33040a720f9cb0a671fc9e2127723fa44c4
Cade <cade@hyperblaster.org>
2015-07-14 09:27:11 +1000

    First commit

---
+++ dist/fluro.video.js
@@ -0,0 +1,191 @@
+
+//Create Fluro UI With dependencies
+angular.module('fluro.video', [
+	'fluro.config',
+    'fluro.util',
+    'youtube-embed'
+	]);
+'use strict';
+
+
+
+angular.module('fluro.video')
+
+
+.directive('fluroVideo', function($compile, Fluro) {
+
+    return {
+        restrict: 'E',
+        replace: true,
+        template: '<div class="fluro-video video-{{model.assetType}}"></div>',
+        controller: 'FluroVideoController',
+        scope: {
+            model: '=ngModel',
+            ngParams: '&',
+        },
+        link: function($scope, $element, $attrs) {
+
+            $scope.params = $scope.ngParams();
+
+            if (!$scope.params) {
+                $scope.params = {
+                    controls: 0,
+                    autoplay: 0,
+                    modestbranding: 1,
+                    playsinline: 1,
+                    showinfo: 0,
+                    theme: 'light',
+                    byline: 0,
+                    portrait: 0,
+                    title: 0
+                }
+            }
+
+
+
+            ///////////////////////////
+
+            var template;
+
+            switch ($scope.model.assetType) {
+                case 'youtube':
+                    template = '<div class="embed-responsive embed-responsive-16by9"><youtube-video class="embed-responsive-item" video-url="model.external.youtube" player-vars="params"/></div>';
+                    break;
+                case 'vimeo':
+                    template = '<div class="embed-responsive embed-responsive-16by9"><vimeo-video class="embed-responsive-item" video-url="model.external.vimeo" player-vars="params"/></div>';
+                    break;
+                case 'upload':
+                    $scope.playUrl = Fluro.apiURL + '/get/' + $scope.model._id;
+                    template = '<div class="embed-responsive embed-responsive-16by9"><video class="embed-responsive-item" controls><source ng-src="{{playUrl | trustfluro}}" type="{{model.mimetype}}"></video></div>';
+                    break;
+            }
+
+            //Create the template
+            if (template) {
+                var cTemplate = $compile(template)($scope);
+                $element.append(cTemplate);
+            }
+        },
+
+
+    };
+})
+
+
+.filter('trustfluro', ['$sce',
+    function($sce) {
+        return function(val) {
+            return $sce.trustAsResourceUrl(val);
+        };
+    }
+])
+
+/////////////////////////////////////////////////////
+
+.controller('FluroVideoController', function($scope) {
+
+
+
+    //console.log('Inline video', $scope.model)
+    // var urlString = $fluro_url + '/get/' + $scope.id;
+
+    //$scope.url = urlString;
+});
+'use strict';
+
+angular.module('fluro.video')
+.service('VimeoEmbedSettings', function($http) {
+
+    var controller = {};
+
+    /////////////////////////
+
+    controller.getVideoInformation = function(vimeoID) {
+        return $http.get('http://vimeo.com/api/v2/video/'+vimeoID+'.output');
+    }
+
+    /////////////////////////
+
+    return controller;
+})
+    .directive('vimeoVideo', function($compile, VimeoEmbedSettings) {
+        return {
+            restrict: 'E',
+            scope: {
+                videoId: '=',
+                videoUrl: '=',
+                playerVars: '='
+            },
+            link: function($scope, $element, $attrs) {
+
+                /////////////////////////////////////
+
+                $scope.getVideoInformation = function() {
+                    if($scope.videoId) {
+                        return VimeoEmbedSettings.getVideoInformation($scope.videoId);
+                    }
+                }
+
+                /////////////////////////////////////
+
+                function getVimeoID(url) {
+                    //Vimeo RegExp
+                    var reg = /https?:\/\/(?:www\.)?vimeo.com\/(?:channels\/(?:\w+\/)?|groups\/([^\/]*)\/videos\/|album\/(\d+)\/video\/|)(\d+)(?:$|\/|\?)/;
+                    var match = url.match(reg);
+                    if (match) {
+                        return match[3];
+                    }
+                }
+
+                /////////////////////////////////////
+
+                //Listen for changes
+                $scope.$watch('videoId + videoUrl + playerVars', function() {
+
+                    
+                    var VideoID;
+
+                    if ($scope.videoId) {
+                        VideoID = $scope.videoId;
+                    } else if ($scope.videoUrl) {
+                        VideoID = getVimeoID($scope.videoUrl);
+                    }
+
+                    //////////////////////////////
+                   
+                    //Parameters
+                    var params = '';
+
+                    if ($scope.playerVars) {
+                        for (var key in $scope.playerVars) {
+                            params += '&' + key + '=' + $scope.playerVars[key];
+                        }
+                    }
+
+                    //Actual URL
+                    $scope.vimeoEmbedURL = '//player.vimeo.com/video/'+ VideoID +'?player_id='+ VideoID + params;
+                })
+
+
+                /////////////////////////////////////
+
+                //The template
+                var template = '<iframe ng-src="{{vimeoEmbedURL | trustVimeo}}" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>';
+
+                //Compile the goodness
+                var cTemplate = $compile(template)($scope);
+
+                //Replace the original tag with our embed
+                $element.replaceWith(cTemplate);
+            }
+        };
+    })
+
+.filter('trustVimeo', ['$sce',
+    function($sce) {
+        return function(val) {
+            return $sce.trustAsResourceUrl(val);
+        };
+    }
+])
+
---
+++ gruntfile.js
@@ -0,0 +1,71 @@
+module.exports = function(grunt) {
+
+    "use strict";
+    require("matchdep").filterDev("grunt-*").forEach(grunt.loadNpmTasks);
+
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+
+        watch: {
+            js: {
+                files: [
+                    'lib/js/**/*.js'
+                ],
+                tasks: ['compile'],
+            },
+            html: {
+                files: [
+                    'lib/html/**/*.html'
+                ],
+                tasks: ['compile'],
+            }
+        },
+        ngtemplates: {
+            'fluro.video': {
+                cwd: './lib/html',
+                src: 'fluro/**/*.html',
+                dest: 'dist/fluro.video-templates.js',
+                options: {
+                    htmlmin: {
+                        collapseBooleanAttributes: true,
+                        collapseWhitespace: true,
+                        removeAttributeQuotes: true,
+                        removeComments: true, // Only if you don't use comment directives! 
+                        removeEmptyAttributes: true,
+                        removeRedundantAttributes: true,
+                        removeScriptTypeAttributes: true,
+                        removeStyleLinkTypeAttributes: true
+                    }
+                }
+            }
+        },
+
+        //Concatenate all the build js files
+        concat: {
+            js: {
+                src: ['lib/js/**/*.js'],
+                dest: 'dist/fluro.video.js',
+            }
+        },
+        uglify: {
+            options: {
+                mangle: false
+            },
+            build: {
+                src: [
+                    'dist/fluro.video.js',
+                    'dist/fluro.video-templates.js'
+                ],
+                dest: 'dist/fluro.video.min.js'
+            }
+        }
+    });
+
+    grunt.registerTask('default', ['watch']);
+    //grunt.registerTask('build', ['copy:build', 'htmlmin:build', 'uglify:build', 'cssmin:build']);
+    grunt.registerTask('compile', ['ngtemplates', 'concat', 'uglify']);
+
+    //'autoprefixer', 'cssmin'
+
+
+};
