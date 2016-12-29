2b2d594b68feb13e14f1f34f69ec7ee002e79ab6
Cade <cade@hyperblaster.org>
2016-12-29 23:49:08 +1100

    fixed formatting

--- dist/fluro.video.js
+++ dist/fluro.video.js
@@ -274,7 +274,7 @@ angular.module('fluro.video').service('VimeoEmbedSettings', ['$http', function($
     /////////////////////////
 
     controller.getVideoInformation = function(vimeoID) {
-        return $http.get('http://vimeo.com/api/v2/video/'+vimeoID+'.output');
+        return $http.get('http://vimeo.com/api/v2/video/' + vimeoID + '.output');
     }
 
     /////////////////////////
@@ -283,77 +283,77 @@ angular.module('fluro.video').service('VimeoEmbedSettings', ['$http', function($
 }]);
 
 angular.module('fluro.video').directive('vimeoVideo', ['$compile', 'VimeoEmbedSettings', function($compile, VimeoEmbedSettings) {
-        return {
-            restrict: 'E',
-            scope: {
-                videoId: '=',
-                videoUrl: '=',
-                playerVars: '='
-            },
-            link: function($scope, $element, $attrs) {
+    return {
+        restrict: 'E',
+        scope: {
+            videoId: '=',
+            videoUrl: '=',
+            playerVars: '='
+        },
+        link: function($scope, $element, $attrs) {
 
-                /////////////////////////////////////
+            /////////////////////////////////////
 
-                $scope.getVideoInformation = function() {
-                    if($scope.videoId) {
-                        return VimeoEmbedSettings.getVideoInformation($scope.videoId);
-                    }
+            $scope.getVideoInformation = function() {
+                if ($scope.videoId) {
+                    return VimeoEmbedSettings.getVideoInformation($scope.videoId);
                 }
-
-                /////////////////////////////////////
-
-                function getVimeoID(url) {
-                    //Vimeo RegExp
-                    var reg = /https?:\/\/(?:www\.)?vimeo.com\/(?:channels\/(?:\w+\/)?|groups\/([^\/]*)\/videos\/|album\/(\d+)\/video\/|)(\d+)(?:$|\/|\?)/;
-                    var match = url.match(reg);
-                    if (match) {
-                        return match[3];
-                    }
-                }
-
-                /////////////////////////////////////
-
-                //Listen for changes
-                $scope.$watch('videoId + videoUrl + playerVars', function() {
-
-                    
-                    var VideoID;
-
-                    if ($scope.videoId) {
-                        VideoID = $scope.videoId;
-                    } else if ($scope.videoUrl) {
-                        VideoID = getVimeoID($scope.videoUrl);
-                    }
-
-                    //////////////////////////////
-                   
-                    //Parameters
-                    var params = '';
-
-                    if ($scope.playerVars) {
-                        for (var key in $scope.playerVars) {
-                            params += '&' + key + '=' + $scope.playerVars[key];
-                        }
-                    }
-
-                    //Actual URL
-                    $scope.vimeoEmbedURL = '//player.vimeo.com/video/'+ VideoID +'?player_id='+ VideoID + params;
-                })
-
-
-                /////////////////////////////////////
-
-                //The template
-                var template = '<iframe ng-src="{{vimeoEmbedURL | trustVimeo}}" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>';
-
-                //Compile the goodness
-                var cTemplate = $compile(template)($scope);
-
-                //Replace the original tag with our embed
-                $element.replaceWith(cTemplate);
             }
-        };
-    }]);
+
+            /////////////////////////////////////
+
+            function getVimeoID(url) {
+                //Vimeo RegExp
+                var reg = /https?:\/\/(?:www\.)?vimeo.com\/(?:channels\/(?:\w+\/)?|groups\/([^\/]*)\/videos\/|album\/(\d+)\/video\/|)(\d+)(?:$|\/|\?)/;
+                var match = url.match(reg);
+                if (match) {
+                    return match[3];
+                }
+            }
+
+            /////////////////////////////////////
+
+            //Listen for changes
+            $scope.$watch('videoId + videoUrl + playerVars', function() {
+
+
+                var VideoID;
+
+                if ($scope.videoId) {
+                    VideoID = $scope.videoId;
+                } else if ($scope.videoUrl) {
+                    VideoID = getVimeoID($scope.videoUrl);
+                }
+
+                //////////////////////////////
+
+                //Parameters
+                var params = '';
+
+                if ($scope.playerVars) {
+                    for (var key in $scope.playerVars) {
+                        params += '&' + key + '=' + $scope.playerVars[key];
+                    }
+                }
+
+                //Actual URL
+                $scope.vimeoEmbedURL = '//player.vimeo.com/video/' + VideoID + '?player_id=' + VideoID + params;
+            })
+
+
+            /////////////////////////////////////
+
+            //The template
+            var template = '<iframe ng-src="{{vimeoEmbedURL | trustVimeo}}" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>';
+
+            //Compile the goodness
+            var cTemplate = $compile(template)($scope);
+
+            //Replace the original tag with our embed
+            $element.replaceWith(cTemplate);
+        }
+    };
+}]);
 
 angular.module('fluro.video').filter('trustVimeo', ['$sce',
     function($sce) {
@@ -361,5 +361,4 @@ angular.module('fluro.video').filter('trustVimeo', ['$sce',
             return $sce.trustAsResourceUrl(val);
         };
     }
-])
-
+]);
